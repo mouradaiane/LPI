64694240374b6ecbf62420318f24b9de4ed55ace
imoretini <imoretini@hotmail.com>
2026-04-28 23:00:41 -0300

    Add Cliente Controller

---
+++ SalaoMaven/src/main/java/org/example/ClienteController.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class ClienteController {
+
+    @FXML
+    private Button btAgendar;
+
+    @FXML
+    private Text lblDataDesejada;
+
+    @FXML
+    private Text lblHorarioDesejado;
+
+    @FXML
+    private Text lblNomeCliente;
+
+    @FXML
+    private TextField txtDataDEsejada;
+
+    @FXML
+    private TextField txtHorarioDesejado;
+
+    @FXML
+    private TextField txtNomeCliente;
+
+    @FXML
+    void Agendar(ActionEvent event) {
+
+    }
+
+}
